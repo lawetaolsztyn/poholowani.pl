7a41e0e4f038b11572b2771811ef5b414c5df748
lawetaolsztyn <lawetaolsztyn@gmail.com>
2025-06-12 15:01:26 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,10 @@
-// src/components/Header.jsx
-
-import './Header.css'; // Importujemy dedykowany CSS dla Header'a
-import logo from '../assets/logo.png'; // <--- KLUCZOWE: Upewnij się, że ta ścieżka do logo jest prawidłowa!
+import './Header.css';
 
 export default function Header({ title = "Wykorzystaj puste przebiegi!", subtitle = "Znajdź lub zaoferuj transport powrotny lawet i busów w całej Europie. Prosto i szybko!" }) {
   return (
     <div className="overlay-header">
-      <img src={logo} alt="Poholowani.pl Logo" className="header-logo" /> {/* Użycie zaimportowanego logo */}
-      <div className="header-text-container"> {/* <-- NOWY KONTENER NA TEKST */}
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-      </div>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
     </div>
   );
 }
