6151a124492f67b4c469bd9574ce6d1238c82d1d
lawetaolsztyn <lawetaolsztyn@gmail.com>
2025-06-12 14:11:24 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 // src/components/Header.jsx
 
 import './Header.css'; // Importujemy dedykowany CSS dla Header'a
+import logo from '../assets/logo.png'; // <--- KLUCZOWE: Upewnij się, że ta ścieżka do logo jest prawidłowa!
 
 export default function Header({ title = "Wykorzystaj puste przebiegi!", subtitle = "Znajdź lub zaoferuj transport powrotny lawet i busów w całej Europie. Prosto i szybko!" }) {
   return (
-    // Ten div.overlay-header otrzyma tło i szerokość z LandingPage.css,
-    // ale styl zawartości (tekstu, logo) będzie z Header.css
     <div className="overlay-header">
-      {/* Upewnij się, że logo jest tutaj i ma klasę */}
-      <img src="/poholowani_logo.png" alt="Poholowani.pl Logo" className="header-logo" />
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+      <img src={logo} alt="Poholowani.pl Logo" className="header-logo" /> {/* Użycie zaimportowanego logo */}
+      <div className="header-text-container"> {/* <-- NOWY KONTENER NA TEKST */}
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
     </div>
   );
 }
