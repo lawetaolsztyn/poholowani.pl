35a947f238dcb2d89467674e923b18c75c8f23ed
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-13 21:28:16 +0200

    Może?

--- vite.config.js
+++ vite.config.js
@@ -12,16 +12,14 @@ export default defineConfig({
     https: true,
     fs: { strict: false },
     historyApiFallback: true,
-    // Dodaj to dla serwera deweloperskiego:
-    port: process.env.PORT || 5173, // Użyj zmiennej PORT, jeśli dostępna, w przeciwnym razie domyślny 5173
-    host: true // To jest kluczowe dla kontenerów, aby nasłuchiwać na wszystkich interfejsach
+    port: 8080, // ZMIEŃ TO: Twardo ustawiamy na 8080
+    host: true
   },
   build: {
     outDir: 'dist'
   },
-  // Dodaj konfigurację dla podglądu/produkcji (vite preview)
   preview: {
-    port: process.env.PORT || 4173, // Użyj zmiennej PORT, jeśli dostępna, w przeciwnym razie domyślny 4173
-    host: true // To jest kluczowe dla kontenerów
+    port: 8080, // ZMIEŃ TO: Twardo ustawiamy na 8080
+    host: true
   }
 })
