4c9e8e5f29cfa78d9ce17bad3e18b93ceaedb51d
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-13 22:49:31 +0200

    test i po nim

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://rzqahfqtbqsmhodzlgzs.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6cWFoZnF0YnFzbWhvZHpsZ3pzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNzUxMzksImV4cCI6MjA2MjY1MTEzOX0.q7sQVXNv51PHkpR69P6G7aU1jQJyGKpeEYKEBGKxBVY";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
