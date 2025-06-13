a5dc8d177291a0ea33b4b631ba869f323c3c15ec
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-14 01:36:55 +0200

    new

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('VITE_SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = " VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6cWFoZnF0YnFzbWhvZHpsZ3pzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNzUxMzksImV4cCI6MjA2MjY1MTEzOX0.q7sQVXNv51PHkpR69P6G7aU1jQJyGKpeEYKEBGKxBVY";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
