6a613c15ca42f1b2a08f5e57382b9be21456a3f5
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-19 18:12:55 +0200

    mapbox

--- src/SearchRoutes.jsx
+++ src/SearchRoutes.jsx
@@ -155,7 +155,7 @@ const HighlightedRoute = React.memo(function HighlightedRoute({ route, isHovered
                         <strong>Do:</strong> {route.to_city?.split(',')[0]}
                     </div>
                     <div style={{ marginBottom: '6px' }}>📅 {route.date}</div>
-                    <div style={{ marginBottom: '6px' }> {route.load_capacity || '–'}</div>
+                    <div style={{ marginBottom: '6px' }}>Pojemność: {route.load_capacity || '–'}</div>
                     <div style={{ marginBottom: '6px' }}> {route.passenger_count || '–'}</div>
                     <div style={{ marginBottom: '6px' }}>🚚 {route.vehicle_type === 'laweta' ? 'Laweta' : 'Bus'}</div>
                        {route.phone && (
