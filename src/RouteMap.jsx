e5e183d33e8682a878c0ecf2b1b19338926ccd0b
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-19 17:40:41 +0200

    mapbox

--- src/RouteMap.jsx
+++ src/RouteMap.jsx
@@ -1,6 +1,8 @@
 import { MapContainer, TileLayer, Polyline, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+
 function FitBounds({ routeData }) {
   const map = useMap();
 
@@ -21,7 +23,12 @@ export default function RouteMap({ routeData }) {
       style={{ height: '600px', width: '100%', marginTop: '30px' }}
       gestureHandling={true}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer
+        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}@2x?access_token=${MAPBOX_TOKEN}`}
+        attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        tileSize={512}
+        zoomOffset={-1}
+      />
       {routeData && (
         <>
           <Polyline
