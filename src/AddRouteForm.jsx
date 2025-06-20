69562db62dc01d8e95436fc2b5725e256084bc5e
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-20 23:56:04 +0200

    b

--- src/AddRouteForm.jsx
+++ src/AddRouteForm.jsx
@@ -261,6 +261,9 @@ function AddRouteForm({ onRouteCreated }) {
         user_id: userId || null,
         browser_token: browserToken || null,
         created_at: new Date().toISOString(), // Standardowe pole created_at
+console.log('Dane ORS Geometry przed stringify:', form.orsGeometry); 
+        console.log('Dane ORS Geometry po stringify:', JSON.stringify(form.orsGeometry));
+
         
         // KLUCZOWE ZMIANY: Zapis geometrii trasy, dystansu i czasu trwania
         // route_geom to kolumna typu geography(LineString, 4326)
