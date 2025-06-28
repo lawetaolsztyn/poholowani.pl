216a8a6094dab087843dd6fb898c16f7e9d29bf5
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-28 14:22:40 +0200

    zakladki

--- src/TransportNaJuz.jsx
+++ src/TransportNaJuz.jsx
@@ -1,17 +1,13 @@
 // src/TransportNaJuz.jsx
 import React from 'react';
 import Navbar from './components/Navbar'; // Upewnij się, że ścieżka do Navbar jest poprawna
-import Header from './components/Header'; // Importujemy komponent Header
 import './TransportNaJuz.css'; // Utworzymy ten plik CSS w kolejnym kroku
 
 export default function TransportNaJuz() {
   return (
     <>
       <Navbar />
-      <Header
-        title="Transport Na Już!"
-        subtitle="Pilne zgłoszenia pomocy drogowej i transportu awaryjnego."
-      />
+      
       <div className="content-container"> {/* Nowy kontener na właściwą treść */}
         {/* Tutaj będzie zawartość Transportu Na Już! */}
         <h2>Pilna Pomoc na Drodze!</h2>
