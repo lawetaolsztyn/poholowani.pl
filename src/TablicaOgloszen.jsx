ad05f1b0af7feaae8e462954346a5627a322d852
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-06-28 14:23:49 +0200

    zakladki

--- src/TablicaOgloszen.jsx
+++ src/TablicaOgloszen.jsx
@@ -1,17 +1,13 @@
 // src/TablicaOgloszen.jsx
 import React from 'react';
 import Navbar from './components/Navbar'; // Upewnij się, że ścieżka do Navbar jest poprawna
-import Header from './components/Header'; // Importujemy komponent Header
 import './TablicaOgloszen.css'; // Utworzymy ten plik CSS w kolejnym kroku
 
 export default function TablicaOgloszen() {
   return (
     <>
       <Navbar />
-      <Header
-        title="Tablica Ogłoszeń"
-        subtitle="Przeglądaj ogłoszenia o potrzebie transportu."
-      />
+      
       <div className="content-container"> {/* Nowy kontener na właściwą treść */}
         {/* Tutaj będzie zawartość Tablicy Ogłoszeń */}
         <h2>Witaj na Tablicy Ogłoszeń!</h2>
