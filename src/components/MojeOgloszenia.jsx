cbf5f8567fc248a0a06b0225e1bb22fcbce8ded9
Dariusz Grzybowski <lawetaolsztyn@gmail.com>
2025-07-05 11:50:05 +0200

    moje trasy

--- src/components/MojeOgloszenia.jsx
+++ src/components/MojeOgloszenia.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './MojeOgloszenia.css'; // Utworzymy ten plik CSS później
+import Navbar from './Navbar';
+
 
 export default function MojeOgloszenia() {
   return (
+<>
+<Navbar />
     <div className="moje-ogloszenia-container">
       <h1>Moje Ogłoszenia</h1>
       <p>Tutaj będą wyświetlane Twoje ogłoszenia.</p>
